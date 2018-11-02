cb7b1b7afc0673ab60684aa67ec7131ec3ae502b
scottmyran <scott.myran@gmail.com>
2018-11-02 09:05:07 -0500

    added registration form (end ch5)

--- app/routes.py
+++ app/routes.py
@@ -2,8 +2,8 @@ from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, login_required, logout_user
 from werkzeug.urls import url_parse
 
-from app import app
-from app.forms import LoginForm
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
 from app.models import User
 
 @app.route('/')
@@ -22,6 +22,22 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', posts=posts)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash("Congratulations, you are not a registered user!")
+        return redirect(url_for('login'))
+    
+    return render_template('register.html', title='Register', form=form)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -44,3 +60,4 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+

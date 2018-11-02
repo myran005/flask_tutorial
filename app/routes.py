10df8cc5e9b378a21fc974c474ca9d75e3859cf1
scottmyran <scott.myran@gmail.com>
2018-11-02 05:59:11 -0500

    navbar template

--- app/routes.py
+++ app/routes.py
@@ -5,5 +5,15 @@ from flask import render_template
 @app.route('/index')
 def index():
     user = {'username' : 'Scott' }
-    return render_template('index.html', title = 'Home', user = user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title = 'Home', user = user, posts=posts)
 

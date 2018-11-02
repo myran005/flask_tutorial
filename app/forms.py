27e5583645e989f1a6c958a73458613cc85b1ff3
scottmyran <scott.myran@gmail.com>
2018-11-02 10:43:03 -0500

    Custom error pages, catch name error on edit profile.

--- app/forms.py
+++ app/forms.py
@@ -20,7 +20,7 @@ class RegistrationForm(FlaskForm):
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user is not None:
-            raise ValidationError('That name is already in use.')
+            raise ValidationError('That username is already in use.')
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
@@ -32,3 +32,12 @@ class EditProfileForm(FlaskForm):
     about_me = TextAreaField('About Me', validators=[Length(min=0, max=140)])
     submit = SubmitField('Submit')
     
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError('That username is already in use.')

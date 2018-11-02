975eedfa5bf012b1565d942f2a6081aa2e087b76
scottmyran <scott.myran@gmail.com>
2018-11-02 08:52:42 -0500

    Require login on index. Redirect with next

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, flash, redirect, url_for, request
-from flask_login import current_user, login_user, login_required
+from flask_login import current_user, login_user, login_required, logout_user
 from werkzeug.urls import url_parse
 
 from app import app
@@ -37,7 +37,7 @@ def login():
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
-            return redirect(next_page)
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/logout')

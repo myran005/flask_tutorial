f514a72d44a3e72cd689fefa6f82b4a3767ec448
scottmyran <scott.myran@gmail.com>
2018-11-02 06:33:37 -0500

    login form

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app
 from flask import render_template
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -15,5 +16,9 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title = 'Home', user = user, posts=posts)
+    return render_template('index.html', title='Home', user=user, posts=posts)
 
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
